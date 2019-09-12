1274e921115c554b5e8f117eadb5cd4a19854a80
surya-ra <ess.dee3107@gmail.com>
2019-09-12 00:23:36 -0700

    linked list using class

---
+++ linked_list.py
@@ -0,0 +1,41 @@
+class Node:
+	def __init__(self, data):
+		self.value = data
+		self.next = None
+
+class LinkedList:
+	def __init__(self):
+		self.head = Node(0)
+
+	def append(self,item):
+		new_node = Node(item)
+		cur = self.head
+		while cur.next != None:
+			cur = cur.next
+		cur.next = new_node
+		
+	def display(self):
+		cur_node = self.head
+		#print(cur_node.next.value)
+		while cur_node and cur_node.next:
+			print(str(cur_node.value) + "->", end = '')
+			cur_node = cur_node.next
+
+		"""
+		while in_node in type_node:
+			ptint(str(in_node.value) + "->", end = '')
+			in_node = in_node.next
+
+		if in_node:
+			print(in_node.value)
+
+		else:
+			print("empty Node")
+		"""
+if __name__ == '__main__':
+	mylist = LinkedList()
+	mylist.append(1)
+	mylist.append(4)
+	mylist.append(3)
+	mylist.display()
+	
